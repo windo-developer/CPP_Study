e9ad5a55bd140f9e5fdfb600f16437f21093e0d1
windo-developer <yui050505@gmail.com>
2021-05-03 11:00:26 +0900

    vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -2,6 +2,77 @@
 using namespace std;
 
 template <typename T>
+class Iterator
+{
+public:
+	Iterator() : _ptr(nullptr)
+	{
+	}
+
+	Iterator(T* ptr) : _ptr(ptr)
+	{
+	}
+
+	Iterator operator+(const int count)
+	{
+		Iterator temp = *this;
+		temp._ptr += count;
+		return temp;
+	}
+
+	Iterator operator-(const int count)
+	{
+		Iterator temp = *this;
+		temp._ptr -= count;
+		return temp;
+	}
+
+	Iterator& operator++()
+	{
+		_ptr++;
+		return *this;
+	}
+
+	Iterator operator++(int)
+	{
+		Iterator temp = this;
+		_ptr++;
+		return temp;
+	}
+
+	Iterator& operator--()
+	{
+		_ptr--;
+		return *this;
+	}
+
+	Iterator operator--(int)
+	{
+		Iterator temp = this;
+		_ptr--;
+		return temp;
+	}
+
+	bool operator==(const Iterator& right)
+	{
+		return _ptr == right._ptr;
+	}
+
+	bool operator!=(const Iterator& right)
+	{
+		return _ptr != right._ptr;
+	}
+
+	T& operator*()
+	{
+		return *_ptr;
+	}
+
+public:
+	T* _ptr;
+};
+
+template <typename T>
 class Vector
 {
 public:
@@ -57,8 +128,18 @@ public:
 		_data = newData;
 	}
 
+	T& operator[] (const int pos)
+	{
+		return _data[pos];
+	}
+
 	int size() { return _size; }
 	int capacity() { return _capacity; }
+
+public:
+	typedef Iterator<T> iterator;
+	iterator begin() { return iterator(&_data[0]); }
+	iterator end() { return begin() + _size; }
 
 private:
 	T* _data;
@@ -68,5 +149,27 @@ private:
 
 int main()
 {
+	Vector<int> v;
+
+	// v.reserve(100);
+
+	for (int i = 0; i < 100; i++)
+	{
+		v.push_back(i);
+		cout << v.size() << " " << v.capacity() << endl;
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+
+	cout << "=====iterator====" << endl;
+
+	for (Vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+	{
+		cout << (*it) << endl;
+	}
+
 	return 0;
 }
